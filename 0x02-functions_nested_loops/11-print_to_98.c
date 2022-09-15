9ead79e043dcf3fa63ab4e8390c61d2c879805a9
Muhammad Hasan <mabdinasirhassan@gmail.com>
2022-09-15 16:10:40 -0700

    Task 11 sol

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print n to 98
+ *
+ * Desctriptiom: number is separated by comma followed by space
+ *
+ * last printed number is 98
+ *
+ * @n: input
+ * return 0
+*/
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+}
