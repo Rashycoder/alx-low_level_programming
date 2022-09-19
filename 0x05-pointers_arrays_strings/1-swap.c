e293e5aecf36f74c3524b594bca2e56dc378c5fd
Muhammad Hasan <mabdinasirhassan@gmail.com>
2022-09-19 15:32:37 -0700

    Task 1 sol

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - Swaps the value of 2 int
+ * @a: An int to swap
+ * @b: an int to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 File zero has a function that take a pointer to an int and update the pointing value
+File 1 has a function that swaps the values of 2 int
