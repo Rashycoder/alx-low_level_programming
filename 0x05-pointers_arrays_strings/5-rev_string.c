e0d2f646b08db5c86fa6734ae77b760272b9fd6b
Muhammad Hasan <mabdinasirhassan@gmail.com>
2022-09-19 16:08:28 -0700

    Task 5 sol

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev-string - Reverse a string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
+
+	while (s[fcounter] != '\0')
+		fcounter++;
+	for (i = 0; i < fcounter; i++)
+	{
+		fcounter--;
+		rev = s[i];
+		s[fcounter] = rev;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ File 1 has a function that swaps the values of 2 int
 File 2 has a function that returns the length of a string
  File 3 contains afunction that a string followed by a new line
  File 4 contains a function that prints string in reverse
+ File 5 has a function that Reverses a string
